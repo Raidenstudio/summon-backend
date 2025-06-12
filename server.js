db5f647764f43b91afeb21e1ed5b4838907c96aa
Harithabaskar06 <harithabaskar0610@gmail.com>
2025-06-12 11:44:16 +0530

    added

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: '*', // or set specifically to 'https://summon-ui.netlify.app'
     methods: ['GET', 'POST'],
-  },
+  }
 });
+
+
 
 const messagesFilePath = path.join(__dirname, 'messages.json');
 
